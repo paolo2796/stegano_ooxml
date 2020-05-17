44743fb6a402cb22f4b5d005dff76f3ca1913cc8
paolo2796 <paolovigorito96@gmail.com>
2020-05-17 12:10:41 +0200

    partial implementation merge run elements

--- encoder.py
+++ encoder.py
@@ -1,38 +1,50 @@
 from lxml import etree
+import xml.etree.ElementTree as ET
 import array
 
+class MisMatchexception(Exception):
+   """Base class for other exceptions"""
+   pass
 
 PREFIX_WORD_PROC = "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}"
 PARAGRAPH_TAG = PREFIX_WORD_PROC + "p"
 RUN_ELEMENT_TAG = PREFIX_WORD_PROC + "r"
 RUN_ELEM_PROPERTY_TAG = PREFIX_WORD_PROC + "rPr"
 BODY_TAG = PREFIX_WORD_PROC + "body"
+TEXT_TAG = PREFIX_WORD_PROC + "t"
+SZCS_TAG = PREFIX_WORD_PROC + "szCs"
 
 
 tree = etree.parse('minimal/word/document.xml')
 root = tree.getroot()
 
 
-
 def merge_possible_run_elements(paragraph):
      #ricerca run elements nel paragrafo
      run_property_elements = paragraph.findall("./" + RUN_ELEMENT_TAG + "/" + RUN_ELEM_PROPERTY_TAG)
+     run_elements_to_merge = []
      i = 0
      for node in run_property_elements:
-         for child_of_node in node:
-            mismatch = False
-            if i + 1 < len(run_property_elements):
-                child_of_node_next = run_property_elements[i + 1].find("./"  + child_of_node.tag)
-                if child_of_node_next != None :
-                    if child_of_node_next.attrib != child_of_node.attrib:
-                        mismatch = True
-                        break
-
-         #endfor
-         i +=1
-
-
-
+        mismatch = False
+        j = i + 1
+        while mismatch != True and j < len(run_property_elements):
+            for child_of_node in node:
+                child_of_node_j = run_property_elements[j].find("./"  + child_of_node.tag)
+                if child_of_node_j == None or (child_of_node.tag!= SZCS_TAG and child_of_node_j.attrib != child_of_node.attrib):
+                    mismatch = True
+                    break
+            if(mismatch != True):
+                j +=1
+            else:
+                j -=1
+        x=i
+        while x < j:
+            run_elements_to_merge.append(paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (x +1).__str__() + "]"))
+            x +=1
+        print(run_elements_to_merge)
+        if(j== len(run_property_elements)):
+            break
+        i +=1
 
 
 
