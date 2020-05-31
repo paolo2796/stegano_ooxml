5c5ca4d4ebaacea5a70a77639f095f742a852f66
paolo2796 <paolovigorito96@gmail.com>
2020-05-31 23:26:31 +0200

    implementazione codifica per paragrafo. (da testare)

--- encoder.py
+++ encoder.py
@@ -1,6 +1,9 @@
 from lxml import etree
 import xml.etree.ElementTree as ET
+
 import array
+import copy
+
 
 class MisMatchexception(Exception):
    """Base class for other exceptions"""
@@ -14,6 +17,9 @@ BODY_TAG = PREFIX_WORD_PROC + "body"
 TEXT_TAG = PREFIX_WORD_PROC + "t"
 SZCS_TAG = PREFIX_WORD_PROC + "szCs"
 
+
+ENCODED_INFORMATION = ""
+ENCODED_INFORMATION_BITS = (''.join(format(ord(x), '#010b') for x in ENCODED_INFORMATION))[2:]
 
 tree = etree.parse('minimal/word/document.xml')
 root = tree.getroot()
@@ -38,7 +44,8 @@ def merge_possible_run_elements(paragraph):
                 while x < j:
                     #append  node i + 1 to base node
                     node = paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (x + 1).__str__() + "]" + "/" + TEXT_TAG)
-                    paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (i + 1).__str__() + "]").append(node)
+                    base_node =  paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (i + 1).__str__() + "]" + "/" + TEXT_TAG)
+                    base_node.text = base_node.text + node.text
                     x += 1
                 x = i + 1
                 while x < j:
@@ -52,7 +59,8 @@ def merge_possible_run_elements(paragraph):
                 while x <= j:
                     #append  node i + 1 to base node
                     node = paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (x + 1).__str__() + "]" + "/" + TEXT_TAG)
-                    paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (i + 1).__str__() + "]").append(node)
+                    base_node =  paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (i + 1).__str__() + "]" + "/" + TEXT_TAG)
+                    base_node.text = base_node.text + node.text
                     x += 1
                 x = i + 1
                 while x <= j:
@@ -63,33 +71,59 @@ def merge_possible_run_elements(paragraph):
             else:
                 j += 1
 
-        tree.write("output.xml")
         if(j== len(run_property_elements)):
             break
         i +=1
 
 
+def shift_run_element_by_pos(paragraph,start):
+    count = len(paragraph.findall("./" + RUN_ELEMENT_TAG))
+    while(count >= start):
+        paragraph.insert(count + 1,copy.copy(paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (count).__str__() + "]")))
+        count -= 1
 
 
 paragraphs = root.findall("./" + BODY_TAG + "/" + PARAGRAPH_TAG)
-for node in paragraphs:
-    merge_possible_run_elements(node)
+i = 0
+for paragraph in paragraphs:
+    merge_possible_run_elements(paragraph)
+    run_elements = paragraph.findall("./" + RUN_ELEMENT_TAG)
+    i_run_elements = 1
+    offset_run_elem = 1
+    while i_run_elements <= len(run_elements):
+        N = 1
+        tag_element = run_elements[i_run_elements - 1].find("./" + TEXT_TAG)
+        count = len(tag_element.text)
+        while count >=1:
+            if(ENCODED_INFORMATION_BITS[i % len(ENCODED_INFORMATION_BITS)]) == "0":
+                N += 1
+            else:
+                tag_element = paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (offset_run_elem).__str__() + "]" + "/" + TEXT_TAG)
+                text = tag_element.text
+                new_run_elem = copy.copy(paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (offset_run_elem).__str__() + "]"))
+                paragraph.insert(offset_run_elem,new_run_elem)
+                offset_run_elem += 1
+                tag_element.text = text[0:N]
+                new_run_elem.find("./" + TEXT_TAG).text = text[N:]
+                N = 1
+            i += 1
+            count -= 1
+        i_run_elements += 1
+        offset_run_elem += 1
+tree.write("output.xml")
+
+
+
+
+
+
+
+
+
+
 exit(0)
 
-'''
-for child in root:
-    if child.tag == BODY_TAG:
-        for child_of_body in child :
-            if child_of_body.tag == PARAGRAPH_TAG :
-                arr_run_elems_property = []
-                for child_of_paragraph in child_of_body :
-                    if(child_of_paragraph.tag == RUN_ELEMENT_TAG) :
-                        for child_of_run_element in child_of_paragraph :
-                            if(child_of_run_element.tag == RUN_ELEM_PROPERTY_TAG ) :
-                                arr_run_elems_property.append(child_of_paragraph)
-                # end  child_of_paragraph
-                merge_possible_run_elements(arr_run_elems_property) 
-'''
+
 
 
 
