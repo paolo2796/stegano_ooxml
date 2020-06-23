6be0375913c7221f991310f23fb3536e822d411d
paolo2796 <paolovigorito96@gmail.com>
2020-06-23 17:36:19 +0200

    implementazione codificatore (da testare).

--- encoder.py
+++ encoder.py
@@ -85,42 +85,60 @@ def shift_run_element_by_pos(paragraph,start):
 
 paragraphs = root.findall("./" + BODY_TAG + "/" + PARAGRAPH_TAG)
 i = 0
+# step 10 -> Repeat Step 2 to Step 9 until all paragraph elements have been addressed.
 for paragraph in paragraphs:
+    #step 3 -> If two or more adjacent run elements in P have the same attributes, merge these run elements
     merge_possible_run_elements(paragraph)
+    tree.write("output.xml")
     run_elements = paragraph.findall("./" + RUN_ELEMENT_TAG)
     i_run_elements = 1
     offset_run_elem = 1
+
+    # rimuovo tutti i nodi != RUN_ELEMENT_TAG e li memorizzo in un array
+    arr_childs_par_to_save = []
+    for child_paragraph in paragraph.findall("./"):
+        if child_paragraph.tag != RUN_ELEMENT_TAG:
+            arr_childs_par_to_save.append(child_paragraph)
+            paragraph.remove(child_paragraph)
+
+
+    #step 4 -> Extract a run element <w:r>…</w:r> to R, corresponding text element <w:t>…</w:t> to T, and the corresponding attributes to A.
     while i_run_elements <= len(run_elements):
         N = 1
         tag_element = run_elements[i_run_elements - 1].find("./" + TEXT_TAG)
+        #step 5 ->Make a count N=1 to accumulate the number of characters to be divided. Count the number of characters in T, and record it to C.
         count = len(tag_element.text)
         while count >=1:
+            #step 6 -> Read one bit from the encoded information H circularly, decrease the value of C by one (in this process, assume the digit “1” is used for splitting):
+            # case a
             if(ENCODED_INFORMATION_BITS[i % len(ENCODED_INFORMATION_BITS)]) == "0":
                 N += 1
+            #case b
             else:
                 tag_element = paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (offset_run_elem).__str__() + "]" + "/" + TEXT_TAG)
                 text = tag_element.text
                 new_run_elem = copy.copy(paragraph.find("./" + RUN_ELEMENT_TAG + "[" + (offset_run_elem).__str__() + "]"))
+                # step 8 -> Modify the splitting mark <w:szCs> in the run elements alternatively.
+                if new_run_elem.find("./" + RUN_ELEM_PROPERTY_TAG + "/" + SZCS_TAG) != None:
+                    new_run_elem.find("./" + RUN_ELEM_PROPERTY_TAG + "/" + SZCS_TAG).set(PREFIX_WORD_PROC + "val","32")
                 paragraph.insert(offset_run_elem,new_run_elem)
                 offset_run_elem += 1
                 tag_element.text = text[0:N]
                 new_run_elem.find("./" + TEXT_TAG).text = text[N:]
+                tree.write("output.xml")
                 N = 1
             i += 1
             count -= 1
+            # step 7 -> Go to step 6 until the value of C is one.
         i_run_elements += 1
         offset_run_elem += 1
+        # step 9 -> Repeat Step 4 to Step 8 until all run elements have been addressed in the paragraph element P
+    # push di tutti i nodi != RUN_ELEMENT_TAG memorizza in arr_childs_par_to_save
+    for item in arr_childs_par_to_save:
+        paragraph.append(item)
+
+
 tree.write("output.xml")
-
-
-
-
-
-
-
-
-
-
 exit(0)
 
 
